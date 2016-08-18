d72f97938be7af127315158ec72bbba50311ab0a
kandoit7 <jmin8884@gmail.com>
2016-08-18 16:30:38 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 
-var audioInputSelect1 = document.querySelector('select.one');
-var audioInputSelect2 = document.querySelector('select.two');
-var audioInputSelect3 = document.querySelector('select.three');
-var audioInputSelect4 = document.querySelector('select.four');
-var selectors = [audioInputSelect1, audioInputSelect2, audioInputSelect3, audioInputSelect4];
+var audioInputSelect1 = document.querySelector('select');
+var selectors = [audioInputSelect1];
 
 function gotDevices(deviceInfos) {
+	var masterInputSelector = document.createElement('select');
+	/*
 	var values = selectors.map(function(select) {
 		return select.value;
 	});
@@ -15,10 +14,12 @@ function gotDevices(deviceInfos) {
 			select.removeChild(select.firstChild);
 		}
 	});
+	*/
 	for (var i = 0; i !== deviceInfos.length; ++i) {
 		var deviceInfo = deviceInfos[i];
 		var option = document.createElement('option');
 		option.value = deviceInfo.deviceId;
+		/*
 		if (deviceInfo.kind === 'audioinput') {
 			option.text = deviceInfo.label ||
 			'microphone ' + (audioInputSelect1.length + 1);
@@ -26,7 +27,16 @@ function gotDevices(deviceInfos) {
 		} else {
 			console.log('Some other kind of source/device: ', deviceInfo);
 		}
+		*/
+		if (deviceInfo.kind === 'audioinput') {
+			option.text = deviceInfo.label ||
+			'microphone ' + (masterInputSelector.length + 1);
+			masterInputSelector.appendChild(option);
+		} else {
+			console.log('Some other kind of source/device: ', deviceInfo);
+		}
 	}
+	/*
 	selectors.forEach(function(select, selectorIndex) {
 		if (Array.prototype.slice.call(select.childNodes).some(function(n) {
 			return n.value === values[selectorIndex];
@@ -34,6 +44,25 @@ function gotDevices(deviceInfos) {
 			select.value = values[selectorIndex];
 		}
 	});
+	*/
+	var audioInputSelect = document.querySelectorAll('select');
+  	for (var selector = 0; selector < audioInputSelect.length; selector++) {
+    		var newInputSelector = masterInputSelector.cloneNode(true);
+    		newInputSelector.addEventListener('change', changeAudioDestination);
+    		audioInputSelect[selector].parentNode.replaceChild(newInputSelector,
+        	audioInputSelect[selector]);
+  	}
+}
+
+function changeAudioDestination(event) {
+	var deviceId = event.target.value;
+	var outputSelector = event.target;
+	var element = event.path[2].childNodes[1];
+	attachSinkId(element, deviceId, outputSelector);
+}
+
+function gotStream(stream) {
+	window.stream = stream; // make stream available to console
 }
 
 function initAudio() {
@@ -47,7 +76,7 @@ function initAudio() {
 	var constraints = {
 		audio: { deviceId: audioSource ? {exact: audioSource} : undefined}
 	};
-	navigator.mediaDevices.getUserMedia(constraints,gotDevices,handleError);
+	navigator.mediaDevices.getUserMedia(constraints).then(gotStream).catch(handleError);
 }
 
 function handleError(error) {
