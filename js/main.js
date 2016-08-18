8d7559b23ab1aa3b9c65fdeeb8a68fda5f181eec
kandoit7 <jmin8884@gmail.com>
2016-08-18 16:09:39 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ function initAudio() {
 	var constraints = {
 		audio: { deviceId: audioSource ? {exact: audioSource} : undefined}
 	};
-	navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
+	navigator.mediaDevices.getUserMedia(constraints).then(gotDevices).catch(handleError);
 }
 
 function handleError(error) {
