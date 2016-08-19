96378bc5453858420a3dc7787da988e992e653ef
kandoit7 <jmin8884@gmail.com>
2016-08-19 17:23:08 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,10 +31,12 @@ function gotBuffers( buffers ) {
 	audioRecorder.exportWAV( doneEncoding );
 }
 
-function play() {
+function play(event) {
 	var link = document.getElementById('save');
 	var tracklink = document.getElementById(lrecord);
 	tracklink.href = link.href
+	var track = new Audio(tracklink.href);
+	track.play();
 }
 function toggleRecording( e ) {
 	canvasID = e.id;
